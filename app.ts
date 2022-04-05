7c4bb32643a736fd0176afcaadf3a153e2662767
Guillaume Coubard <guillaume.coubard@yahoo.fr>
2022-04-05 16:16:07 +0200

    Script implémentant le mouvement des aiguilles en fonction de l'heure locale à l'aide d'un setInterval

--- app.ts
+++ app.ts
@@ -8,3 +8,21 @@ for(let i=1; i<=12; i++){
     span.style.setProperty('--i', i.toString())
     clock?.appendChild(span)
 }
+
+
+const hours = document.querySelector('#hours') as HTMLDivElement
+const minutes = document.querySelector('#minutes') as HTMLDivElement
+const secondes = document.querySelector('#secondes') as HTMLDivElement
+
+
+setInterval(()=>{
+    const date  = new Date()
+    let hh = date.getHours() * 30
+    let min = date.getMinutes() * 6
+    let sec = date.getSeconds() * 6
+
+    hours.style.transform = `rotate(${hh+(min/12)}deg)`
+    minutes.style.transform = `rotate(${min}deg)`
+    secondes.style.transform = `rotate(${sec}deg)`
+
+}, 1000)
--- dist/app.js
+++ dist/app.js
@@ -8,3 +8,15 @@ for (let i = 1; i <= 12; i++) {
     span.style.setProperty('--i', i.toString());
     clock === null || clock === void 0 ? void 0 : clock.appendChild(span);
 }
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const secondes = document.querySelector('#secondes');
+setInterval(() => {
+    const date = new Date();
+    let hh = date.getHours() * 30;
+    let min = date.getMinutes() * 6;
+    let sec = date.getSeconds() * 6;
+    hours.style.transform = `rotate(${hh + (min / 12)}deg)`;
+    minutes.style.transform = `rotate(${min}deg)`;
+    secondes.style.transform = `rotate(${sec}deg)`;
+}, 1000);
